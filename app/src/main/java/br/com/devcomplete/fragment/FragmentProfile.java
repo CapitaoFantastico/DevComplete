7e6104d18499d10205c6ed2124f869fb7c5c3a06
Henrique Souza <henriquesouza182@gmail.com>
2016-07-20 00:01:13 -0300

    Created Screen for CRUD  Validate fields fill - Name Field

--- app/src/main/java/br/com/devcomplete/fragment/FragmentProfile.java
+++ app/src/main/java/br/com/devcomplete/fragment/FragmentProfile.java
@@ -2,10 +2,13 @@ package br.com.devcomplete.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import br.com.devcomplete.R;
 
@@ -13,6 +16,10 @@ import br.com.devcomplete.R;
  * Created by ricks on 14/07/2016.
  */
 public class FragmentProfile extends Fragment{
+
+    private Button btnRegister;
+    private TextInputLayout lytTxtName;
+    private TextView txtName;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +29,32 @@ public class FragmentProfile extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false); //Inflate First, everything in my fragment are hear!
+
+        btnRegister = (Button) view.findViewById(R.id.btnRegister);
+        lytTxtName = (TextInputLayout) view.findViewById(R.id.lytTxtName);
+        txtName = (TextView) view.findViewById(R.id.edtName);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!validateName()){
+                    return;
+                }
+            }
+        });
+
+        return view;
     }
-}
+
+        private boolean validateName(){
+            if(txtName.getText().toString().trim().isEmpty()){
+                lytTxtName.setError("The name field is required!");
+                return false;
+            } else {
+                lytTxtName.setErrorEnabled(false);
+            }
+                return true;
+        }
+    }
